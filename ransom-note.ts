8df0e41529c42320a917e4d2293321a5b926461d
Harrison Cramer <harrisoncramer@gmail.com>
2024-10-06 13:27:54 -0400

    Updated ransom-note.ts

--- ransom-note.ts
+++ ransom-note.ts
@@ -1,25 +1,25 @@
 import { test } from "./_test"
 
-function ransomNote (s1: string, s2: string): boolean {
+function ransomNote (note: string, sourceChars: string): boolean {
+  if(note.length === 0) return true
 
   // Build a map that of char => # of occurrences
   const charMap = new Map()
-  for(const char of s1) {
+  for(const char of note) {
     let currentVal = charMap.get(char)
     charMap.set(char, (currentVal || 0) + 1)
   }
 
   // Iterate through all source characters. 
-  // If we find a character we need (>0 in our map), then reduce
-  // by one.
+  // If we find a character we need (>0 in our map), then reduce by one.
   // If we found all our characters then return true
   let foundChars = 0
-  for(const char of s2) {
+  for(const char of sourceChars) {
     const remaining = charMap.get(char)
     if (remaining > 0) {
       charMap.set(char, charMap.get(char) - 1)
       foundChars++
-      if (foundChars === s1.length) return true
+      if (foundChars === note.length) return true
     }
   }
 
@@ -32,6 +32,18 @@ const testCases = [
   {
     input: ['banana', 'aome sbnanp aancakes'],
     want: true
+  },
+  {
+    input: ['', 'blah'],
+    want: true
+  },
+  {
+    input: ['cat', 'sdiofacjioat'],
+    want: true
+  },
+  {
+    input: ['dog', 'sdiofacjioat'],
+    want: false
   }
 ]
 
