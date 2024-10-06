f5eafff9e338daba89db8a0428a67478e07e2520
Harrison Cramer <harrisoncramer@gmail.com>
2024-10-06 13:33:24 -0400

    Updated two-sum.ts

--- two-sum.ts
+++ two-sum.ts
@@ -1,5 +1,7 @@
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
+import { test } from "./_test"
+
 function twoSum (nums: number[], target: number): [number, number]|null {
   const intMap = {}
   for(const [i, num] of nums.entries()) {
@@ -11,32 +13,19 @@ function twoSum (nums: number[], target: number): [number, number]|null {
   return null
 }
 
-type TestCase = {
-  input: [number[], number][]
-  want: [number, number]
-}
-
 const testCases = [
   {
-    arr: [3,10,2,20,18,7,39],
-    target: 38,
+    input: [[3,10,2,20,18,7,39], 38],
     want: [3,4],
   },
   {
-    arr: [3,10,2,20,18,7,39],
-    target: 46,
+    input: [[3,10,2,20,18,7,39], 46],
     want: [5,6],
   },
   {
-    arr: [3,10,5,8],
-    target: 100,
+    input: [[3,10,5,8], 100],
     want: null
   }
 ]
 
-for (const [i, testCase] of testCases.entries()) {
-  const got = twoSum(testCase.arr, testCase.target)  
-  if (JSON.stringify(testCase.want) !== JSON.stringify(got)) {
-    throw new Error(`Test case ${i} failed: Got ${got} but wanted ${testCase.want}`)
-  }
-}
+test(testCases, twoSum)
