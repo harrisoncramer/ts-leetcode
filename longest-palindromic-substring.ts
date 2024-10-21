0c626532ea67f358377d9204979dc8b363ecf208
Harrison Cramer <harrisoncramer@gmail.com>
2024-10-20 18:15:51 -0700

    Updated longest-palindromic-substring.ts

--- longest-palindromic-substring.ts
+++ longest-palindromic-substring.ts
@@ -1,40 +1,48 @@
 /* Write a function that finds the longest palindromic substring in a string.
 
-Solution: The key insight here is that when building a palindrome, you can do it from the inside out. 
+Solution: The key insight here is that when building a palindrome, you can do it from the inside out. Iterate over the input string. For each loop, we need to check whether you can build a palindrome with the current value (i) as the middle value. This can be done with a helper method that checks whether left and right are the same character, and if they are, moves them outward. The condition exits when left or right goes out of bounds.
 
-Iterate over the input string. For each loop, create two pointers, before and after, which are before and after the index. While those pointers are in bounds, march them outwards. Each time they move, check to see if the characters they point at are the same. If so, add both characters to the left/right side of the result. 
+This will work for strings like "aba" or "racecar" since they have a middle letter.
 
-If one of them is the same as the middle character, then add it to the result. This is to handle cases where you have repeated characters, but there isn't an odd number so you don't compare the left and right, e.g. "aabc"
+For even lengthed strings, start the right pointer one further out, since that will cause this type of a pointer progression:
 
-Time Complexity: You have to do O(n) operations for each character, and there are O(m) characters in the overall string, so you end up with O(n^2) time complexity. Imagine a string with all the same character
+         l r
+- "c m a a a a d"
+
+       l     r
+- "c m a a a a d"
+
+     l         r
+- "c m a a a a d"
+
+
+Time Complexity: You have to do O(n) operations for each character, and there are O(n) characters in the overall string, so you end up with O(n^2) time complexity. Imagine a string with all the same character.
 Space Complexity: O(n) because you are building up a string whose maximum length is O(n).
 */
 
 import { test } from "./_test";
 
-function longestPalindromicSubstring (str: string): string {
-  if(str.length <= 1) return str
-
-  let result = str[0]
-
-  for (let i = 0; i < str.length; i++) {
-    let before = i - 1;
-    let after = i + 1;
-    let currentStr = str[i]
-    while(str[before] && str[after]) {
-      
-      if(str[before] === str[after]) { // Two surrounding characters are the same
-        currentStr = `${str[before]}${currentStr}${str[after]}`
-      } else if (str[before] === currentStr || str[after] === currentStr) { // The char is the same as the string before it or after it, e.g. "aabc"
-        currentStr += currentStr
-      }
-
-      if (currentStr.length > result.length) result = currentStr
-      before--
-      after++
-    }
+function checkSubstring (left: number, right: number, s: string, result: string): string {
+  let res = ""
+  while (left >= 0 && right < s.length && s[left] === s[right]) {
+    res = s.substring(left, right + 1) // Add both left and right
+    left--
+    right++
   }
 
+  return res.length > result.length ? res : result
+}
+
+function longestPalindrome(s: string): string {
+  let result = ""
+  for (let i = 0; i < s.length; i++) {
+
+    /* Optimistically update with odd length palindromes */
+    result = checkSubstring(i, i, s, result)
+
+    /* Optimistically update w/ even-numbered palindromes, move right pointer one further */
+    result = checkSubstring(i, i + 1, s, result)
+  }
   return result
 }
 
@@ -44,12 +52,19 @@ const testCases = [
     want: "racecar",
   },
   {
-    input: ["zracecare"],
+    input: ["bb"],
+    want: "bb",
+  },
+  { input: ["zracecare"],
     want: "racecar"
   },
   {
     input: ["abcdeff"],
     want: "ff",
+  },
+  {
+    input: ["aaaa"],
+    want: "aaaa",
   },
   {
     input: ["aabc"],
@@ -62,7 +77,11 @@ const testCases = [
   {
     input: ["aaab"],
     want: "aaa",
+  },
+  {
+    input: ["caaaaa"],
+    want: "aaaaa",
   }
 ]
 
-test(testCases, longestPalindromicSubstring)
+test(testCases, longestPalindrome)
