dba0f4e3e6f177a466469a0f8bb1a1f8bc7fce06
Harrison Cramer <harrisoncramer@gmail.com>
2024-10-25 19:20:24 -0400

    Updated permutations.ts

--- permutations.ts
+++ permutations.ts
@@ -1,9 +1,7 @@
 /*
 Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
 
-Solution: You are essentially building up a path here. Create a recursive function that checks to see if the current path is equal in length to the expected output. If it is, then add it to the result, and return.
-
-If not, iterate over each value in the nums and check to see if it's already in the path. If it's not in the path already, push it in and then call the helper function with the expanded path. At the end, make sure to pop() off the value, so that the next loop isn't affected.
+Solution: You are essentially building up a path here. Create a recursive function that passes down the path. Each time, check to see if the current path is equal in length to the expected output. If it is, then add it to the result, and return. If not, iterate over each value in the nums (not the path!) and check to see if it's already in the path. If it's not in the path already, push it in and then call the helper function with the expanded path. Afterward, don't forget to pop() the value off the path again, so that the next iteration of the loop doesn't have it.
 
 Time Complexity: O(n * n!) there are n! unique combinations (n - 1)(n - 2)(etc..) and copying each permutation to the result (spread) taekes n time to accomplish. This is the optimal time complexity.
 
@@ -25,7 +23,7 @@ function permute(nums: number[]): number[][] {
       if (!path.includes(x)) {
         path.push(x);
         helper(path);
-        path.pop();
+        path.pop(); // Do not forget to pop off the path
       }
     }
   }
